a30df41231c274e2a11b4acfdb64954b7918db49
Artemiy Mutovin <artemmutovin@mail.ru>
2022-11-28 04:38:59 -0500

    Разделение пользователей на покупателей и продавцов, добавление аттрибутов first_name, last_name, type к модели User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,7 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
+
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
 end
