467eb3a5b2045b00c860fcb54357b9766665f179
Artemiy Mutovin <artemmutovin@mail.ru>
2023-01-07 07:08:26 -0500

    изменения в миграции AddDeviseToUsers, добавление файла database.yml.sample

--- db/migrate/20221212093637_add_devise_to_users.rb
+++ db/migrate/20221212093637_add_devise_to_users.rb
@@ -37,8 +37,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.timestamps null: false
     end
 
-    remove_column( :users, :password_digest)
-    change_column_default( :users, :email, '')
+    #remove_column( :users, :password_digest)
+    #change_column_default( :users, :email, '')
 
     # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
@@ -52,7 +52,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
                     :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at,
                     :confirmation_sent_at, :unconfirmed_email)
 
-    add_column :users, :password_digest, :string
-    change_column_default( :users, :email, nil)
+    #add_column :users, :password_digest, :string
+    #change_column_default( :users, :email, nil)
   end
 end
