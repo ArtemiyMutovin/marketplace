af5965ab6c4685fe5b318e7b47ab60976654a3b5
Artemiy Mutovin <artemmutovin@mail.ru>
2023-01-07 08:05:05 -0500

    Удаление проверки авторизации у пользователей

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, unless: :devise_controller?
-
-  authorize_resource
 
   def index
     @products = Product.all
