a6183273a7e015b27b79f0f10345cbe0efa16312
Artemiy Mutovin <artemmutovin@mail.ru>
2023-01-07 07:29:47 -0500

    Удаление ненужных записей в миграции AddDeviseToUsers

--- db/migrate/20221212093637_add_devise_to_users.rb
+++ db/migrate/20221212093637_add_devise_to_users.rb
@@ -37,9 +37,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.timestamps null: false
     end
 
-    #remove_column( :users, :password_digest)
-    #change_column_default( :users, :email, '')
-
     # add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
@@ -51,8 +48,5 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
                     :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at,
                     :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at,
                     :confirmation_sent_at, :unconfirmed_email)
-
-    #add_column :users, :password_digest, :string
-    #change_column_default( :users, :email, nil)
   end
 end
