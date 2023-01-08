788a5e8d8b72e1ea4baede6c24b7cb4a7a464134
Artemiy Mutovin <artemmutovin@mail.ru>
2023-01-08 07:10:32 -0500

    rubocop

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -8,15 +8,29 @@ RSpec.describe Ability, type: :model do
     let(:other) { create :seller, :other, company: company }
     let(:company) { create(:company) }
     let(:category) { create(:category) }
-    let(:product) { create :product, seller: user, category: category }
+    let(:product) { create :product, seller: user, category: category, company: company }
 
     it { is_expected.not_to be_able_to :manage, :all }
 
-    it { is_expected.to be_able_to :update, create(:product, seller: user, category: category), seller: user }
-    it { is_expected.not_to be_able_to :update, create(:product, seller: other, category: category), seller: user }
+    it {
+      expect(subject).to be_able_to :update, create(:product, seller: user, category: category, company: company),
+                                    seller: user
+    }
 
-    it { is_expected.to be_able_to :destroy, create(:product, seller: user, category: category), seller: user }
-    it { is_expected.not_to be_able_to :destroy, create(:product, seller: other, category: category), seller: user }
+    it {
+      expect(subject).not_to be_able_to :update, create(:product, seller: other, category: category, company: company),
+                                        seller: user
+    }
+
+    it {
+      expect(subject).to be_able_to :destroy, create(:product, seller: user, category: category, company: company),
+                                    seller: user
+    }
+
+    it {
+      expect(subject).not_to be_able_to :destroy, create(:product, seller: other, category: category, company: company),
+                                        seller: user
+    }
   end
 
   describe 'user' do
