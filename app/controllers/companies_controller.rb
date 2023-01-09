122f010444b9038c676a379bf7e0cbb0de235184
Artemiy Mutovin <artemmutovin@mail.ru>
2023-01-09 08:45:04 -0500

    Форма для создания компании

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,33 @@
 class CompaniesController < ApplicationController
   before_action :company, only: %i[show]
+
+  def index
+    @companies = Company.all
+  end
+
   def show; end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
 
   private
 
   def company
     @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name)
   end
 end
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -6,9 +6,26 @@ RSpec.describe CompaniesController, type: :controller do
   let(:product) { create :product, category: category, seller: other, company: company }
   let(:category) { create(:category) }
 
+  describe 'GET #index' do
+    let(:companies) { create_list(:company, 3) }
+
+    before do
+      allow(Company).to receive(:all).and_return(companies)
+      get :index
+    end
+
+    it 'populates an array of all companies' do
+      expect(assigns(:companies)).to match_array(companies)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
   describe 'GET #show' do
     before do
-      get :show, params: { id: product }
+      get :show, params: { id: company }
     end
 
     it 'assigns the requested company to @company' do
@@ -17,6 +34,46 @@ RSpec.describe CompaniesController, type: :controller do
 
     it 'renders show view' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      get :new, params: { company_id: company }
+    end
+
+    it 'assigns the requested company to @company' do
+      expect(assigns(:company)).to be_a_new(Company)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new company in database' do
+        expect { post :create, params: { company: attributes_for(:company) } }
+          .to change(Company, :count).by(1)
+      end
+
+      it 'redirects to company' do
+        post :create, params: { company: attributes_for(:company) }
+        expect(response).to redirect_to company_path(assigns(:company))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not saves a new company in database' do
+        expect { post :create, params: { company: attributes_for(:company, :invalid) } }
+          .not_to change(Company, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { company: attributes_for(:company, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
